57293e91fabc5b3645fae4c5242c2099958b5a0c
gongxb <gxb2260@126.com>
2018-07-25 01:07:17 +0800

    feature <chapter2 hi> 支持hystrix

--- chapter2/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
+++ chapter2/service-hi/src/main/java/com/forezp/ServiceHiApplication.java
@@ -1,9 +1,15 @@
 package com.forezp;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
 public class ServiceHiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceHiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
 
-	@Value("${server.port}")
-	String port;
-	@RequestMapping("/hi")
-	public String home(@RequestParam String name) {
-		return "hi "+name+",i am from port:" +port;
-	}
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name) {
+        System.out.println(System.currentTimeMillis() +"：："+name);
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+    @RequestMapping("/info")
+    public String home() {
+        return "hi ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
 
 }
